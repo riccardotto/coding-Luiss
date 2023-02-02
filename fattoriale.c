3228ea9c2584401b6899f2dd4444b22e9174188c
riccardotto <124038324+riccardotto@users.noreply.github.com>
2023-02-02 09:39:36 +0100

    Add files via upload

---
+++ fattoriale.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void fattoriale(int n)
+{
+    
+}
+
+int main()
+{
+    int n;
+    int tot=2;
+    printf("inserisci un numero\n");
+    scanf("%d",&n);
+    while(n>0)
+
+    {
+        tot=tot*n;
+        n=n-1;}
+        printf("finale%d",tot);
+    
+    
+    
+
+    return 0;
+}
